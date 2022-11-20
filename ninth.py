7753c97e6a633ea27e14da65b5c700ee12b9f408
SHEEVAN <104026386+SheevanUlHaq@users.noreply.github.com>
2022-11-20 17:26:48 +0530

    Dictionary

---
+++ ninth.py
@@ -0,0 +1,39 @@
+def percentage(dict):
+    for item in dict:
+        ds = dict[item]["DS"]
+        cn = dict[item]["CN"]
+        python = dict[item]["Python"]
+        os = dict[item]["OS"]
+
+
+def maxPercentage(dict):
+    pass
+
+
+myDict = {
+    "Rahul": {
+        "DS": 75,
+        "CN": 68,
+        "Python": 83,
+        "OS": 67
+    },
+    "Sameer": {
+        "DS": 92,
+        "CN": 73,
+        "Python": 55,
+        "OS": 53
+    },
+    "Saim": {
+        "DS": 70,
+        "CN": 72,
+        "Python": 65,
+        "OS": 83
+    },
+    "Harsh": {
+        "DS": 59,
+        "CN": 66,
+        "Python": 79,
+        "OS": 72
+    }
+
+}
